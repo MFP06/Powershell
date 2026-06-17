c3b41c94aab89637980e23b573d329fdb0f449c7
Mathéo Falcomer <m.falcomer06@gmail.com>
2026-06-17 16:57:36 +0200

    commit

---
+++ Lancer-Dealabs.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "%~dp0"
+
+if not exist "Interface_graphique.ps1" (
+    echo Erreur : Interface_graphique.ps1 est introuvable dans ce dossier.
+    echo Dossier courant : %CD%
+    echo.
+    echo Fichiers PowerShell disponibles :
+    dir /b *.ps1
+    echo.
+    pause
+    exit /b 1
+)
+
+powershell.exe -ExecutionPolicy Bypass -File "%CD%\Interface_graphique.ps1"
+
